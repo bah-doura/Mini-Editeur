0f70544567f9c02fe3ce2d72e8e6e82105572090
romain <rboitard@gmail.com>
2017-11-30 20:16:44 +0100

    Update Coller enregistrable

--- src/mementos/MementoColler.java
+++ src/mementos/MementoColler.java
@@ -2,14 +2,6 @@ package mementos;
 
 public class MementoColler implements Memento {
 
-    private String texte;
-
-    public MementoColler(String texte) {
-        this.texte = texte;
-    }
-
-    public String getTexte()
-    {
-        return this.texte;
+    public MementoColler() {
     }
 }
--- src/originators/CommandeEnregistrableColler.java
+++ src/originators/CommandeEnregistrableColler.java
@@ -1,36 +1,42 @@
 package originators;
 
+import caretaker.ConcreteEnregistreur;
+import caretaker.Enregistreur;
 import commandes.Commande;
 import mementos.Memento;
 import mementos.MementoColler;
+import moteurEdition.MoteurEdition;
 
 public class CommandeEnregistrableColler implements Enregistrable, Commande {
 
-    private String texte;
+    private MoteurEdition moteurEdition;
+    private Enregistreur enregistreur;
 
-    public CommandeEnregistrableColler()
+    public CommandeEnregistrableColler( MoteurEdition moteurEdition, Enregistreur enregistreur)
     {
-        this.texte ="";
+        this.moteurEdition = moteurEdition;
+        this.enregistreur = enregistreur;
     }
     @Override
     public void execute() {
-
+        if(this.enregistreur.isRecording()){
+            this.enregistreur.addMemento(storInMemento(), this);
+        }
+        else if(this.enregistreur.isReplay()){
+            this.restoreFromMemento(enregistreur.getCurrentMemento());
+        }
+        this.moteurEdition.coller();
     }
 
     @Override
     public Memento storInMemento() {
-        return new MementoColler(this.texte);
+        return new MementoColler();
 
     }
 
     @Override
     public void restoreFromMemento(Memento memento) {
         MementoColler mementoColler = (MementoColler)memento;
-        this.texte = mementoColler.getTexte();
     }
 
-    public void setTexte(String texte)
-    {
-        this.texte = texte;
-    }
 }
