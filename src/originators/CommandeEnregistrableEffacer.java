06b9355c71640067a01f9ee05e76e05d783372ef
romain <rboitard@gmail.com>
2017-11-30 20:17:30 +0100

    Update effacer enregistrable

--- src/originators/CommandeEnregistrableEffacer.java
+++ src/originators/CommandeEnregistrableEffacer.java
@@ -1,14 +1,30 @@
 package originators;
 
+import caretaker.Enregistreur;
 import commandes.Commande;
 import mementos.Memento;
 import mementos.MementoEffacer;
+import moteurEdition.MoteurEdition;
 
 public class CommandeEnregistrableEffacer implements Enregistrable, Commande {
 
+    private MoteurEdition moteurEdition;
+    private Enregistreur enregistreur;
+
+    public CommandeEnregistrableEffacer(MoteurEdition moteurEdition, Enregistreur enregisteur){
+        this.moteurEdition = moteurEdition;
+        this.enregistreur = enregistreur;
+    }
+
     @Override
     public void execute() {
-
+        if(this.enregistreur.isRecording()){
+            this.enregistreur.addMemento(this.storInMemento(), this);
+        }
+        else if(this.enregistreur.isReplay()){
+            this.restoreFromMemento(this.enregistreur.getCurrentMemento());
+        }
+        this.moteurEdition.effacer();
     }
 
     @Override
