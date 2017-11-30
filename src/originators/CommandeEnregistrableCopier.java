8a8e823573e5844eef5a2b839ee052db30989b13
romain <rboitard@gmail.com>
2017-11-30 20:16:57 +0100

    Update Copier enregistrable

--- src/originators/CommandeEnregistrableCopier.java
+++ src/originators/CommandeEnregistrableCopier.java
@@ -1,11 +1,21 @@
 package originators;
 
+import caretaker.Enregistreur;
 import commandes.Commande;
 import mementos.Memento;
 import mementos.MementoCopier;
+import moteurEdition.MoteurEdition;
 
 public class CommandeEnregistrableCopier implements Enregistrable, Commande {
 
+
+    private MoteurEdition moteurEdition;
+    private Enregistreur enregistreur;
+
+    public CommandeEnregistrableCopier(Enregistreur enregistreur, MoteurEdition moteurEdition){
+        this.moteurEdition = moteurEdition;
+        this.enregistreur = enregistreur;
+    }
 
     @Override
     public Memento storInMemento() {
@@ -19,6 +29,12 @@ public class CommandeEnregistrableCopier implements Enregistrable, Commande {
 
     @Override
     public void execute() {
-
+        if(this.enregistreur.isRecording()){
+            this.enregistreur.addMemento(this.storInMemento(), this);
+        }
+        else if(this.enregistreur.isReplay()){
+            this.restoreFromMemento(this.enregistreur.getCurrentMemento());
+        }
+        this.moteurEdition.copier();
     }
 }
