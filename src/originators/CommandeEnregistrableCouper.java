eef6477a9a374d555dc7e278710d3e01b838e328
romain <rboitard@gmail.com>
2017-11-30 20:17:19 +0100

    Update Couper enregistrable

--- src/originators/CommandeEnregistrableCouper.java
+++ src/originators/CommandeEnregistrableCouper.java
@@ -1,14 +1,30 @@
 package originators;
 
+import caretaker.Enregistreur;
 import commandes.Commande;
 import mementos.Memento;
 import mementos.MementoCouper;
+import moteurEdition.MoteurEdition;
 
 public class CommandeEnregistrableCouper implements Enregistrable, Commande {
 
+    private MoteurEdition moteurEdition;
+    private Enregistreur enregistreur;
+
+    public CommandeEnregistrableCouper(MoteurEdition moteurEdition, Enregistreur enregistreur){
+        this.moteurEdition = moteurEdition;
+        this.enregistreur = enregistreur;
+    }
+
     @Override
     public void execute() {
-
+        if(this.enregistreur.isRecording()){
+            this.enregistreur.addMemento(this.storInMemento(), this);
+        }
+        else if(this.enregistreur.isReplay()){
+            this.restoreFromMemento(this.enregistreur.getCurrentMemento());
+        }
+        this.moteurEdition.couper();
     }
 
     @Override
