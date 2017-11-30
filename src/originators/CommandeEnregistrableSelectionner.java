8949f0af43f71fe4a0356c70b6437f99c8ad88b6
romain <rboitard@gmail.com>
2017-11-30 20:17:58 +0100

    Update Selectionner enregistrable

--- src/originators/CommandeEnregistrableSelectionner.java
+++ src/originators/CommandeEnregistrableSelectionner.java
@@ -1,22 +1,38 @@
 package originators;
 
+import IHM.Controleur;
+import caretaker.Enregistreur;
 import commandes.Commande;
 import mementos.Memento;
 import mementos.MementoSelectionner;
+import moteurEdition.MoteurEdition;
 import moteurEdition.Selection;
 
 public class CommandeEnregistrableSelectionner implements Enregistrable, Commande {
 
+    private MoteurEdition moteurEdition;
+    private Enregistreur enregistreur;
+    private Controleur controleur;
     Selection selection;
 
-    public CommandeEnregistrableSelectionner()
+    public CommandeEnregistrableSelectionner(MoteurEdition moteurEdition, Enregistreur enregistreur, Controleur controleur)
     {
+        this.moteurEdition = moteurEdition;
+        this.enregistreur = enregistreur;
+        this.controleur = controleur;
         this.selection = new Selection();
     }
 
     @Override
     public void execute() {
-
+        this.setSelection(controleur.getDebutSelection(),controleur.getFinSelection());
+        if(this.enregistreur.isRecording()){
+            this.enregistreur.addMemento(this.storInMemento(), this);
+        }
+        else if(this.enregistreur.isReplay()){
+            this.restoreFromMemento(this.enregistreur.getCurrentMemento());
+        }
+        this.moteurEdition.selectionnerTexte(controleur.getDebutSelection(),controleur.getFinSelection());
     }
 
     @Override
