5f975b3bd61a583c701be1b2b19f73d82dab83e6
romain <rboitard@gmail.com>
2017-11-30 20:17:43 +0100

    Update InsererText enregistrable

--- src/originators/CommandeEnregistrableInsererText.java
+++ src/originators/CommandeEnregistrableInsererText.java
@@ -1,21 +1,37 @@
 package originators;
 
+import IHM.Controleur;
+import caretaker.Enregistreur;
 import commandes.Commande;
 import mementos.Memento;
 import mementos.MementoInsererTexte;
+import moteurEdition.MoteurEdition;
 
 public class CommandeEnregistrableInsererText implements Enregistrable, Commande {
 
     private String texte;
+    private MoteurEdition moteurEdition;
+    private Enregistreur enregistreur;
+    private Controleur controleur;
 
-    public CommandeEnregistrableInsererText()
+    public CommandeEnregistrableInsererText(MoteurEdition moteurEdition, Enregistreur enregistreur, Controleur controleur)
     {
+        this.controleur = controleur;
+        this.moteurEdition = moteurEdition;
+        this.enregistreur = enregistreur;
         this.texte = "";
     }
 
     @Override
     public void execute() {
-
+        this.setTexte(controleur.getText());
+        if(this.enregistreur.isRecording()){
+            this.enregistreur.addMemento(this.storInMemento(), this);
+        }
+        else if(this.enregistreur.isReplay()){
+            this.restoreFromMemento(this.enregistreur.getCurrentMemento());
+        }
+        this.moteurEdition.insererTexte(this.texte);
     }
 
     @Override
